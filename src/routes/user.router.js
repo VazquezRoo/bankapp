61aa704a42a13dc1edcff44a4b9ed58be92b57e0
Cristian Vazquez <vazquez_roo@outlook.com>
2023-06-08 17:43:28 -0700

    update

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -5,10 +5,10 @@ const usercontrollers = require('../controllers/user.controllers');
 
 const router = express.Router();
 
-router.route('signup').post(usercontrollers.createUser);
+router.route('/signup').post(usercontrollers.createUser);
 
-router.route('login').post(usercontrollers.login);
+router.route('/login').post(usercontrollers.login);
 
-// router.route(":id/history").get();
+router.route('/:id/history').get();
 
 module.exports = router;
